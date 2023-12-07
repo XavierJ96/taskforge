64290e86662cdef5d3817ea58d394a6ecab22aaf
Xavier Julies <78542365+XavierJ96@users.noreply.github.com>
2023-12-07 13:42:38 +0200

    chore: initial commit

---
+++ old_work/contentScript copy.js
@@ -0,0 +1,80 @@
+console.log("Notion Tasks Extension content script loaded");
+
+function getCardData() {
+  //Get column data
+  const parentElement = document.querySelector(
+    "#root > div > main > div.jss12 > div.MuiGrid-root.MuiGrid-container.MuiGrid-wrap-xs-nowrap"
+  );
+
+  if (parentElement) {
+    const children = parentElement.children;
+
+    Array.from(children).forEach((child, index) => {
+      const columnNameElement = child.querySelector("div > h5");
+
+      if (columnNameElement) {
+        const columnName = columnNameElement.textContent.trim();
+
+        // Check if the column name includes "Complete"
+        if (!columnName.includes("Complete")) {
+          const cardElements = child.querySelectorAll(
+            "div.MuiPaper-root.MuiCard-root > div.MuiCardContent-root > h2"
+          );
+          const cards = child.querySelectorAll(
+            "div.MuiPaper-root.MuiCard-root"
+          );
+
+          Array.from(cards).forEach((card, cardIndex) => {
+            const cardTitleElement = card.querySelector(
+              "div.MuiCardContent-root > h2"
+            );
+            const cardTitle = cardTitleElement.textContent.trim();
+
+            const addButton = document.createElement("button");
+            addButton.classList.add("MuiChip-root", "jss367");
+            addButton.style.padding = "5px 10px";
+            addButton.style.margin = "10px";
+            addButton.style.cursor = "pointer";
+            addButton.textContent = "➕ Add to Planner";
+
+            addButton.addEventListener("click", () => {
+              chrome.runtime.sendMessage({
+                action: "addTaskToPlanner",
+                cardTitle: cardTitle,
+                cardType: getCardType(card),
+                taskId: card.dataset.taskId,
+              });
+            });
+
+            card.appendChild(addButton);
+          });
+        }
+      } else {
+        console.warn(`Column name not found for child ${index + 1}`);
+      }
+    });
+  } else {
+    return null;
+  }
+}
+
+function getCardType(cardElement) {
+  const backgroundColor = window
+    .getComputedStyle(cardElement)
+    .getPropertyValue("background-color");
+
+  if (
+    backgroundColor === "rgb(187, 222, 251)" ||
+    backgroundColor === "rgb(238, 238, 238)"
+  ) {
+    return "project";
+  } else if (backgroundColor === "rgb(255, 224, 178)") {
+    return "review";
+  } else {
+    return "unknown";
+  }
+}
+
+setTimeout(function () {
+  getCardData();
+}, 10000);
---
+++ popup.js
@@ -0,0 +1,110 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Retrieve the details from Chrome Storage
+  chrome.storage.local.get(["projectTasks", "reviewTasks"], function (result) {
+    const projectTasks = result.projectTasks || [];
+    const reviewTasks = result.reviewTasks || [];
+
+    // Update the project and review task lists in the popup HTML
+    updateTaskList("projectList", projectTasks);
+    updateTaskList("reviewList", reviewTasks);
+  });
+
+  // Add click event listener to the "Delete All Tasks" button
+  const deleteAllTasksButton = document.getElementById("deleteAllTasksButton");
+  if (deleteAllTasksButton) {
+    deleteAllTasksButton.addEventListener("click", deleteAllTasks);
+  }
+});
+
+function updateTaskList(listId, tasks) {
+  const listElement = document.getElementById(listId);
+
+  // Clear existing list items
+  listElement.innerHTML = "";
+
+  // Add new list items based on the received tasks
+  if (tasks.length === 0) {
+    const noTasksMessage = document.createElement("li");
+    noTasksMessage.textContent = "No tasks available.";
+    listElement.appendChild(noTasksMessage);
+  } else {
+    tasks.forEach((task) => {
+      const listItem = document.createElement("li");
+
+      listItem.textContent = task.type === "review" ? `${task.title} by ${task.assignee}`: task.title;
+
+      // Create a delete button for each task
+      const deleteButton = document.createElement("button");
+      deleteButton.innerHTML =
+        '<img src="svg/delete-icon.svg" alt="Delete Icon">';
+      deleteButton.classList.add("deleteButton");
+      deleteButton.style.margin = "0 10px"
+      deleteButton.addEventListener("click", () => {
+        // Handle task deletion
+        deleteTask(task);
+      });
+
+      // Append the delete button to the list item
+      listItem.appendChild(deleteButton);
+
+      // Append the list item to the task list
+      listElement.appendChild(listItem);
+    });
+  }
+}
+
+function deleteTask(taskToDelete) {
+  // Retrieve the tasks from Chrome Storage
+  chrome.storage.local.get(["projectTasks", "reviewTasks"], function (result) {
+    let taskIndex;
+
+    if (taskToDelete.type === "project") {
+      taskIndex = result.projectTasks.findIndex(
+        (projectTask) =>
+          projectTask.title === taskToDelete.title &&
+          projectTask.type === taskToDelete.type
+      );
+
+      if (taskIndex !== -1) {
+        result.projectTasks.splice(taskIndex, 1);
+      }
+    } else if (taskToDelete.type === "review") {
+      taskIndex = result.reviewTasks.findIndex(
+        (reviewTask) =>
+          reviewTask.title === taskToDelete.title &&
+          reviewTask.type === taskToDelete.type
+      );
+
+      if (taskIndex !== -1) {
+        result.reviewTasks.splice(taskIndex, 1);
+      }
+    }
+
+    chrome.storage.local.set(
+      { projectTasks: result.projectTasks, reviewTasks: result.reviewTasks },
+      function () {
+        // Update the task lists in the popup HTML
+        updateTaskList("projectList", result.projectTasks);
+        updateTaskList("reviewList", result.reviewTasks);
+      }
+    );
+  });
+}
+
+function deleteAllTasks() {
+  // Retrieve the tasks from Chrome Storage
+  chrome.storage.local.get(["projectTasks", "reviewTasks"], function (result) {
+    // Clear all tasks
+    result.projectTasks = [];
+    result.reviewTasks = [];
+
+    // Update the task lists in local storage
+    chrome.storage.local.set(result, function () {
+      console.log("All tasks deleted");
+
+      // Update the task lists in the popup HTML
+      updateTaskList("projectList", []);
+      updateTaskList("reviewList", []);
+    });
+  });
+}
