04a0559070f2a75dfc4a5e28513b11db1c6b8744
Xavier Julies <78542365+XavierJ96@users.noreply.github.com>
2023-12-08 19:44:05 +0200

    update: add checkbox

--- src/components/taskCard.js
+++ src/components/taskCard.js
@@ -1,6 +1,19 @@
 const createListItem = (task) => {
   const listItem = document.createElement("li");
 
+  listItem.addEventListener("change", function (event) {
+    const checkbox = event.target;
+    const container = document.getElementById("card-container");
+
+    if (checkbox.type === "checkbox") {
+      if (checkbox.checked) {
+        container.classList.add("checked");
+      } else {
+        container.classList.remove("checked");
+      }
+    }
+  });
+
   const assigneeDiv = task.assignee
     ? `<div id="list-assignee">Assignee: ${task.assignee}</div>`
     : "";
@@ -8,6 +21,10 @@ const createListItem = (task) => {
   listItem.className = "list-item";
   listItem.innerHTML = `  
   <div id="card-container">
+  <label class="checkbox">
+    <input type="checkbox" class="checkbox__input" />
+    <span class="checkbox__inner"></span>
+  </label>
   <div id="list-container">
     <div id="list-header">${task.title}</div>
    ${assigneeDiv}
@@ -18,6 +35,5 @@ const createListItem = (task) => {
     </button>
   </div>
     `;
-
   return listItem;
 };
