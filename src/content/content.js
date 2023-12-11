2dd355f550d361dd37a87f2fabcfe806cf6ee663
Xavier Julies <78542365+XavierJ96@users.noreply.github.com>
2023-12-11 13:56:41 +0200

    chore: change images to assets

--- src/content/content.js
+++ src/content/content.js
@@ -1,3 +1,51 @@
+function addButtonToCard(card, cardTitle, cardType, cardAssignee) {
+  const addButton = document.createElement("button");
+  addButton.classList.add("MuiChip-root", "jss367");
+  addButton.style.padding = "5px 10px";
+  addButton.style.margin = "10px";
+  addButton.style.cursor = "pointer";
+  addButton.textContent = "➕ Add to Planner";
+
+  addButton.addEventListener("click", () => {
+    chrome.runtime.sendMessage({
+      action: "addTaskToPlanner",
+      cardTitle: cardTitle,
+      cardType: cardType,
+      ...(cardType === "review" && { cardAssignee: cardAssignee }),
+    });
+  });
+
+  card.appendChild(addButton);
+}
+
+// Function to process each card and its details
+function processCard(child, index) {
+  const columnNameElement = child.querySelector("div > h5");
+
+  if (columnNameElement) {
+    const columnName = columnNameElement.textContent.trim();
+
+    if (!columnName.includes("Complete")) {
+      const cards = child.querySelectorAll("div.MuiPaper-root.MuiCard-root");
+
+      Array.from(cards).forEach((card, cardIndex) => {
+        const cardTitleElement = card.querySelector("div.MuiCardContent-root > h2");
+        const cardAssigneeElement = card.querySelector("div.MuiCardContent-root > p");
+
+        const cardTitle = cardTitleElement.textContent.trim();
+        const cardAssignee = cardAssigneeElement.textContent.trim();
+
+        const cardType = getCardType(card);
+
+        addButtonToCard(card, cardTitle, cardType, cardAssignee);
+      });
+    }
+  } else {
+    console.warn(`Column name not found for child ${index + 1}`);
+  }
+}
+
+// Main function to get card data
 function getCardData() {
   const parentElement = document.querySelector(
     "#root > div > main > div.jss12 > div.MuiGrid-root.MuiGrid-container.MuiGrid-wrap-xs-nowrap"
@@ -7,51 +55,7 @@ function getCardData() {
     const children = parentElement.children;
 
     Array.from(children).forEach((child, index) => {
-      const columnNameElement = child.querySelector("div > h5");
-
-      if (columnNameElement) {
-        const columnName = columnNameElement.textContent.trim();
-
-        if (!columnName.includes("Complete")) {
-          const cards = child.querySelectorAll(
-            "div.MuiPaper-root.MuiCard-root"
-          );
-
-          Array.from(cards).forEach((card, cardIndex) => {
-            const cardTitleElement = card.querySelector(
-              "div.MuiCardContent-root > h2"
-            );
-            const cardAssigneeElement = card.querySelector(
-              "div.MuiCardContent-root > p"
-            );
-
-            const cardTitle = cardTitleElement.textContent.trim();
-            const cardAssignee = cardAssigneeElement.textContent.trim();
-
-            const addButton = document.createElement("button");
-            addButton.classList.add("MuiChip-root", "jss367");
-            addButton.style.padding = "5px 10px";
-            addButton.style.margin = "10px";
-            addButton.style.cursor = "pointer";
-            addButton.textContent = "➕ Add to Planner";
-
-            addButton.addEventListener("click", () => {
-              chrome.runtime.sendMessage({
-                action: "addTaskToPlanner",
-                cardTitle: cardTitle,
-                cardType: getCardType(card),
-                ...(getCardType(card) === "review" && {
-                  cardAssignee: cardAssignee,
-                }),
-              });
-            });
-
-            card.appendChild(addButton);
-          });
-        }
-      } else {
-        console.warn(`Column name not found for child ${index + 1}`);
-      }
+      processCard(child, index);
     });
   } else {
     return null;
