4ef8bb124ef019a28ff5320c005126437e52f657
Xavier Julies <78542365+XavierJ96@users.noreply.github.com>
2024-02-11 19:01:42 +0200

    feat: add button for manual tasks

---
+++ src/components/AddTasks.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faCalendar } from "@fortawesome/free-solid-svg-icons";
+
+function AddTasks() {
+  const [taskText, setTaskText] = useState("");
+  const [taskDate, setTaskDate] = useState("");
+
+  const handleTextChange = (event) => {
+    setTaskText(event.target.value);
+  };
+
+  const handleDateChange = (event) => {
+    setTaskDate(event.target.value);
+  };
+
+  const handleDateIconClick = () => {
+    const currentDate = new Date().toISOString().split("T")[0];
+    setTaskDate(currentDate);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const task = {
+      text: taskText,
+      date: taskDate,
+    };
+    console.log(task);
+    setTaskText("");
+    setTaskDate("");
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="absolute bottom-0 w-full bg-[#161616] min-h-[50px] p-3 flex items-center space-x-3"
+    >
+      <div className="relative w-full">
+        <FontAwesomeIcon
+          icon={faPlus}
+          className="absolute left-3 top-[50%] -translate-y-[50%] text-white cursor-pointer"
+        />
+        <input
+          type="text"
+          className="pl-8 border w-full bg-[#161616] text-white border-gray-300 px-3 py-2 mr-2 rounded-lg"
+          value={taskText}
+          placeholder="Add Tasks"
+          onChange={handleTextChange}
+        />
+      </div>
+      <input
+        type="date"
+        required
+        className="border w-[150px] bg-[#161616] text-white border-gray-300 px-2 py-2 text-[11px] rounded-lg"
+        value={taskDate}
+        onChange={handleDateChange}
+      />
+    </form>
+  );
+}
+
+export default AddTasks;
