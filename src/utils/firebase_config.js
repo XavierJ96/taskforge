bb3b530d11cb9e09992f0ef286e9c55ce89a272b
Xavier Julies <78542365+XavierJ96@users.noreply.github.com>
2024-06-27 18:55:36 +0200

    chore: config updated

--- src/utils/firebase_config.js
+++ src/utils/firebase_config.js
@@ -9,7 +9,7 @@ export const table = new Airtable({
 }).base(import.meta.env.VITE_AIRTABLE_BASE);
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "PUT_IN_A_DUMMY_API_KEY",
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
